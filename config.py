611995131a6f1e97475a6fd91290d9f2c1d14808
kbu922 <2353614yuanyuan@kbu.ac.kr>
2025-06-29 15:10:19 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+
 DB_CONFIG = {
     'host': 'localhost',
-    'port': 3306,  # 이 포트가 MySQL 설정과 동일해야 함
-    'user': 'your_username',
-    'password': 'your_password',
-    'database': 'your_database'
+    'user': 'root',
+    'password': 'Kbuhui@12',
+    'database': 'dormitory',
+    'port': 3306
 }
